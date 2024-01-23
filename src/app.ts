fbd1bc340aca206b17ce08a4a7e578454fe0870b
unknown <williamfurtunato@hotmail.com>
2024-01-23 17:22:16 -0300

    chore: preparar para deploy

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import fastify from "fastify";
-import cookie from "@fastify/cookie";
-import { transactionsRoutes } from "./routes/transactions";
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { transactionsRoutes } from './routes/transactions'
 
-export const app = fastify();
+export const app = fastify()
 
-app.addHook("preHandler", async (request) => {
-  console.log(`[${request.method}] ${request.url}`);
-});
+app.addHook('preHandler', async (request) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
 
-app.register(cookie);
+app.register(cookie)
 app.register(transactionsRoutes, {
-  prefix: "transactions",
-});
+  prefix: 'transactions',
+})
